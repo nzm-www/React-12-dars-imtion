adfa7e5870e54fb1dcdbb7b7c15f2df4ef2a3095
nzm-www <nizomiddinabdusattorov50@gmail.com>
2024-08-09 01:19:38 +0500

    e

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,77 +1,106 @@
-import {Input} from 'antd';
-import React, { useState } from 'react';
-import '../styles/home.css'
+import { Input } from "antd";
+import React, { useState } from "react";
+import "../styles/home.css";
 
 function Home() {
-
-  const [arr , setArr] = useState([])
-  const [item , setItem] = useState ({
-    'name' : '',
-    'email' : '',
-    'phone' : '',
-    'avatar' : ''
-  })
-  const [newArr , setNewArr] = useState([])
+  const [arr, setArr] = useState([]);
+  const [item, setItem] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    avatar: "",
+  });
+  const [newArr, setNewArr] = useState([]);
 
   const handle = (e) => {
-    const newItem ={...item}
+    const newItem = { ...item };
     newItem[e.target.id] = e.target.value;
     setItem(newItem);
     console.log(item);
-    
-  }
+  };
 
   const addItemToArr = (user) => {
     setArr((prev) => {
-      return [...prev , user]
-    })
+      return [...prev, user];
+    });
     console.log(arr);
-    localStorage.setItem('Items' , JSON.stringify(arr))
-  }
+    localStorage.setItem("Items", JSON.stringify(arr));
+  };
 
-  function handleSubmit (e) {
+  function handleSubmit(e) {
     e.preventDefault();
-    addItemToArr(item)
+    addItemToArr(item);
   }
-  let items = localStorage.getItem('Items')
+  let items = localStorage.getItem("Items");
 
-  function removeUser (index) {
-    const users = JSON.parse(items)
-    users.slice(index , 1)
+  function removeUser(index) {
+    const users = JSON.parse(items);
+    users.slice(index, 1);
     setNewArr(users);
-    localStorage.removeItem('Items')
-    localStorage.setItem('Items' , JSON.stringify(newArr))
+    localStorage.removeItem("Items");
+    localStorage.setItem("Items", JSON.stringify(newArr));
   }
 
   return (
-    <div className='home'>
+    <div className="home">
       <div className="home-left p-5">
-        <form action="" onSubmit={handleSubmit} className='home-form d-flex flex-column gap-2'>
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className="home-form d-flex flex-column gap-2"
+        >
           <div className="fullname d-flex flex-column gap-2">
             <div className="name">
               <label htmlFor="">Name :</label>
-              <Input onChange={(e)=>handle(e)} value={item.name}  id='name' placeholder='Enter the name' />
+              <Input
+                onChange={(e) => handle(e)}
+                value={item.name}
+                id="name"
+                placeholder="Enter the name"
+              />
             </div>
           </div>
           <div className="email">
             <label htmlFor="">Email :</label>
-            <Input onChange={(e)=>handle(e)} value={item.email} id='email' type='email' placeholder='Enter the email' />
+            <Input
+              onChange={(e) => handle(e)}
+              value={item.email}
+              id="email"
+              type="email"
+              placeholder="Enter the email"
+            />
           </div>
           <div className="Phone number">
             <label htmlFor="">Phone number :</label>
-            <Input onChange={(e)=>handle(e)} value={item.phone} placeholder='Enter phone number' id='phone' />
+            <Input
+              onChange={(e) => handle(e)}
+              value={item.phone}
+              placeholder="Enter phone number"
+              id="phone"
+            />
           </div>
           <div className="image d-flex flex-column gap-1">
             <label htmlFor="">Upload avatar</label>
-            <Input onChange={(e)=>handle(e)} id='avatar' value={item.avatar} placeholder='Paste avatar url' />
+            <Input
+              onChange={(e) => handle(e)}
+              id="avatar"
+              value={item.avatar}
+              placeholder="Paste avatar url"
+            />
           </div>
           <div className="submit">
-            <button className='btn btn-primary w-100 mt-3' onClick={(e)=>handleSubmit(e)}>Submit</button>
+            <button
+              className="btn btn-primary w-100 mt-3"
+              onClick={(e) => handleSubmit(e)}
+            >
+              Submit
+            </button>
           </div>
         </form>
       </div>
       <div className="home-right">
-          {JSON.parse(items)?.map((item , index) => { return (
+        {JSON.parse(items)?.map((item, index) => {
+          return (
             <div className="card">
               <div className="image">
                 <img src={item.avatar} alt="" />
@@ -86,10 +115,16 @@ function Home() {
                 <p>{item.phone}</p>
               </div>
               <div className="btns">
-                <button onClick={()=>removeUser(index)} className="btn btn-danger">Delete</button>
+                <button
+                  onClick={() => removeUser(index)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
               </div>
             </div>
-          )})}
+          );
+        })}
       </div>
     </div>
   );
